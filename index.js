c583d90f7f0cf7172f5a721fa8a13e3c5ba6784a
Renato <renatogomess@protonmail.com>
2023-11-19 11:56:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+diretorio = __dirname + '/trator/'
+
+
+
+app.get('*', function(req, res){
+	rota = diretorio + req.params[0]	
+	
+	fs.access(rota, fs.constants.F_OK, (err) => {
+  	if (err) {
+
+    		res.status(404).send('Not Found');} 
+
+  	else if (rota != '/') {
+
+	    	res.sendFile(rota);}
+	
+	else {
+
+		res.sendFile(diretorio + "index.html")
+
+	}
+	
+	});})
+
+app.use((req, res) => {
+
+   res.status(403).send('unauthorized');
+
+});
+
+app.listen(3000,function(){
+    console.log("Servidor ativo no porta 3000");
+});
+
