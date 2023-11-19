db7084c79d76a0a5e7cb30a9ad001edf8c5d48a3
Renato <renatogomess@protonmail.com>
2023-11-19 12:19:34 -0300

    new

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const express = require('express');
+
 const app = express();
-const fs = require('fs');
 
-diretorio = __dirname + '/mirko/'
-
+dir = __dirname + '/mirko/'
 
 
 app.get('*', function(req, res){
-	rota = diretorio + req.params[0]	
-	
-	fs.access(rota, fs.constants.F_OK, (err) => {
-  	if (err) {
-
-    		res.status(404).send('Not Found');} 
-
-  	else if (rota != '/') {
-
-	    	res.sendFile(rota);}
-	
+	route = req.params[0]	
+	if (route != '/'){
+		res.sendFile(dir + route)}	
 	else {
-
-		res.sendFile(diretorio + "index.html")
-
+		res.sendFile(dir + 'index.html')
 	}
 	
-	});})
+	})
+
 
 
 
 app.listen(3000,function(){
     console.log("Servidor ativo no porta 3000");
 });
+
 
